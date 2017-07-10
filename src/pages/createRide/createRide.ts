2077501aa2b998afa2ca9ecccfe2b6a91d63e8b3
Abdul Basit <abasit@barefoot-studio.be>
2017-07-10 13:08:39 +0500

    geolocater implemented successfully

--- src/pages/createRide/createRide.ts
+++ src/pages/createRide/createRide.ts
@@ -9,37 +9,37 @@ import { NativeGeocoder, NativeGeocoderReverseResult} from '@ionic-native/native
   templateUrl: 'createRide.html'
 })
 export class createRidePage {
-  public position:string;
-  public address:string;
+  public position: string;
+  public address: string;
   constructor(public navCtrl: NavController, private geolocation: Geolocation, private nativeGeocoder: NativeGeocoder) {
 
-}
+  }
 
-getCurrLocation(){
-this.geolocation.getCurrentPosition().then((resp) => {
- console.log(resp.coords.latitude);
- console.log(resp.coords.longitude);
- this.position = resp.coords.latitude + "|" + resp.coords.longitude;
-
-
-}).catch((error) => {
-  console.log('Error getting location', error);
-});
+  getCurrLocation() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      console.log(resp.coords.latitude);
+      console.log(resp.coords.longitude);
+      this.position = resp.coords.latitude + "|" + resp.coords.longitude;
 
 
-
-}
-
-getAddress()
-{
-this.nativeGeocoder.reverseGeocode(33.5857793,73.0877285)
-  .then((result: NativeGeocoderReverseResult) => {
-  this.address = result.street;
-  console.log('The address is ' + result.street + ' in ' + result.countryCode)
-  }).catch((error: any) => console.log(error));
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
 
 
 
-}
+  }
+
+  getAddress() {
+    this.nativeGeocoder.reverseGeocode(33.5857793, 73.0877285)
+      .then((result: NativeGeocoderReverseResult) => {
+        this.address = result.street + ", " + result.district + ", " + result.city + ", " + result.countryName;
+        console.log("Result -> ", result);
+        console.log('The address is ' + result.street + ' in ' + result.countryCode)
+      }).catch((error: any) => console.log(error));
+
+
+
+  }
 
 }
